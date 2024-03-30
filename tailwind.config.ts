6da83695a19d030a6e986232674e830bb7920dc7
Davidhero <owogogahhero@outlook.com>
2024-03-30 13:06:56 +0100

    roadmap

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,7 @@ const config: Config = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/tw-elements-react/dist/js/**/*.js",
   ],
   darkMode: "class",
   theme: {
@@ -81,7 +82,10 @@ const config: Config = {
       },
     },
   },
-  plugins: [addVariablesForColors],
+  plugins: [
+    addVariablesForColors,
+    require("tw-elements-react/dist/plugin.cjs"),
+  ],
 };
 export default config;
 
